feb30aa32f48383d8ba568311c2b187ea141db51
Michelle Setúbal <missetubal@outlook.com>
2022-03-26 14:52:46 -0300

    feat: add procts image in 2 columns

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, ScrollView } from 'react-native';
+import api from '../../service/api';
+import { Container, Image } from './styles';
 
 // import { Container } from './styles';
 
 const Products: React.FC = () => {
-  return <View />;
+  const [category, setCategory] = useState([]);
+    useEffect(() => {
+        api.get('products')
+            .then(data => setCategory(data.data))
+            .catch(error => console.log(error))
+    }, [])
+    const formatUrl = (url: String) => url.replace('http', 'https');
+
+  return (
+    <Container>
+        {/* <ScrollView 
+            showsHorizontalScrollIndicator={false}
+        >
+            {
+                category?.map(item => (
+                    <Container>
+                        <Image source={{ uri: formatUrl(item.image) }} />
+                    </Container>
+                ))
+            }
+        </ScrollView> */}
+        <FlatList
+          data={category}
+          keyExtractor={item => item.image}
+          numColumns={2}
+          renderItem={({ item }) => {
+            return (
+              // category?.map(item => (
+                <Container>
+                    <Image source={{ uri: formatUrl(item.image) }} />
+                </Container>
+            );
+          }}
+        />
+
+    </Container>
+  );
 }
 
 export default Products;
