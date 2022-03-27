7b86b352e2f6fe74d809bf098bff189f32227810
Michelle Setúbal <missetubal@outlook.com>
2022-03-27 16:44:40 -0300

    chore: add svg icons

---
+++ src/assets/more.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Path, Svg } from 'react-native-svg';
+
+function More() {
+    return (
+        <Svg width="14" height="14" viewBox="0 0 14 14" fill="none">
+            <Path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="white" />
+        </Svg>
+    );
+};
+
+export default More;
